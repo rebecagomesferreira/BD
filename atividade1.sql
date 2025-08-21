6a9e84397310c99a8623c5356c60530a575bd3c1
rebecagomesferreira <rebecagomes2208@gmail.com>
2025-08-20 21:24:55 -0300

    Update atividade1.sql
    versão0.1

--- atividade1.sql
+++ atividade1.sql
@@ -1,21 +1,5 @@
+CREATE DATABASE atividade1;
 USE atividade1;
-
--- DROP TABLE IF EXISTS autor;
-
-CREATE TABLE IF NOT EXISTS autor(
-ID_AUTOR int primary key
-);
-
-INSERT INTO autor(id_autor)
-VALUES
-(12345), 
-(32415),
-(54321);
-
-SELECT id_autor
-FROM autor;
-
--- DROP TABLE IF EXISTS pessoa;
 
 CREATE TABLE IF NOT EXISTS pessoa(
 CPF varchar(11) primary key,
@@ -32,7 +16,73 @@ VALUES
 SELECT cpf, nome, data_nascimento
 FROM pessoa;
 
--- DROP TABLE IF EXISTS livro;
+CREATE TABLE IF NOT EXISTS curso(
+ID_CURSO int primary key,
+NOME varchar(250)
+);
+
+INSERT INTO curso(id_curso, nome)
+VALUES
+('982', 'Engenharia de Software'),
+('198', 'Engenharia Eletrica'),
+('123', 'Engenharia Civil');
+
+SELECT id_curso, nome
+FROM curso;
+
+CREATE TABLE IF NOT EXISTS turma(
+codigo_turma int primary key,
+semestre varchar(50),
+ID_CURSO int,
+CONSTRAINT CURSO_ID
+FOREIGN KEY (ID_CURSO)
+REFERENCES curso(ID_CURSO)
+);
+
+INSERT INTO turma(codigo_turma, semestre, id_curso)
+VALUES
+('5454545', 'segundo', '982'),
+('9898989', 'terceiro', '198'),
+('2323232', 'quarto', '123');
+
+SELECT codigo_turma, semestre, id_curso
+FROM turma;
+
+CREATE TABLE IF NOT EXISTS aluno(
+MATRICULA int AUTO_INCREMENT PRIMARY KEY,
+ENTRADA char(7) ,
+ATIVO int,
+ID_CURSO int,
+codigo_turma int,
+CONSTRAINT CURSO_FK
+FOREIGN KEY (ID_CURSO) 
+REFERENCES curso(ID_CURSO),
+CONSTRAINT ID_TURMA
+FOREIGN KEY (codigo_turma)
+REFERENCES turma(codigo_turma)
+);
+
+INSERT INTO aluno(matricula, entrada, ativo, id_curso, codigo_turma)
+VALUES 
+(NULL, '2024-01', '1', '982', '2323232'),
+(NULL,'2024-02', '1', '198', '5454545'),
+(NULL,'2025-01', '0', '123', '9898989');
+
+SELECT matricula, entrada, ativo, id_curso, codigo_turma
+FROM aluno;
+
+CREATE TABLE IF NOT EXISTS autor(
+ID_AUTOR int primary key
+);
+
+INSERT INTO autor(id_autor)
+VALUES
+(12345), 
+(32415),
+(54321);
+
+SELECT id_autor
+FROM autor;
 
 CREATE TABLE IF NOT EXISTS livro(
 ISBN int primary key,
@@ -54,19 +104,15 @@ VALUES
 SELECT isbn, id_autor, titulo, ano, editora
 FROM livro;
 
--- DROP TABLE IF EXISTS emprestimo;
-
 CREATE TABLE IF NOT EXISTS emprestimo(
 ID_EMPRESTIMO int primary key,
 DATA_EMPRESTIMO date,
 DATA_DEVOLUCAO date,
 ISBN int,
 MATRICULA int,
-CONSTRAINT LIVRO_ISBN
-FOREIGN KEY (ISBN)
+CONSTRAINT LIVRO_ISBN FOREIGN KEY (ISBN)
 REFERENCES livro(ISBN),
-CONSTRAINT MATRICULA_ALUNO
-FOREIGN KEY (MATRICULA)
+CONSTRAINT ALUNO_MATRICULA FOREIGN KEY(MATRICULA)
 REFERENCES aluno(MATRICULA)
 );
 
@@ -75,67 +121,6 @@ VALUES
 ('7463536', '902378222', '1', '2025-08-13', '2025-08-27'),
 ('9947237', '938483499', '2', '2025-08-15', '2025-08-30'),
 ('0292727', '948326444', '3', '2025-08-10', '2025-08-25');
-SELECT id_emprestimo, data_emprestimo, data_devolucao
+SELECT id_emprestimo, isbn, matricula, data_emprestimo, data_devolucao
+
 FROM emprestimo;
-
--- DROP TABLE IF EXISTS aluno;
--- DROP TABLE IF EXISTS curso;
-
-CREATE TABLE IF NOT EXISTS curso(
-ID_CURSO int primary key,
-NOME varchar(250)
-);
-
-INSERT INTO curso(id_curso, nome)
-VALUES
-('982', 'Engenharia de Software'),
-('198', 'Engenharia Eletrica'),
-('123', 'Engenharia Civil');
-
-SELECT id_curso, nome
-FROM curso;
-
-CREATE TABLE IF NOT EXISTS aluno(
-MATRICULA int AUTO_INCREMENT PRIMARY KEY,
-ENTRADA char(7) ,
-ATIVO int,
-ID_CURSO int,
-CONSTRAINT CURSO_ID
-FOREIGN KEY (ID_CURSO) 
-REFERENCES curso(ID_CURSO)
-);
-
-INSERT INTO aluno(matricula, entrada, ativo, id_curso)
-VALUES 
-(NULL, '2024-01', '1', '982'),
-(NULL,'2024-02', '1', '198'),
-(NULL,'2025-01', '0', '123');
-
-SELECT matricula, entrada, ativo, id_curso
-FROM aluno;
-
--- DROP TABLE IF EXISTS turma;
-
-CREATE TABLE IF NOT EXISTS turma(
-codigo_turma int primary key,
-semestre varchar(50)
-);
-
-INSERT INTO turma(codigo_turma, semestre)
-VALUES
-('5454545', 'segundo'),
-('9898989', 'terceiro'),
-('2323232', 'quarto');
-
-SELECT codigo_turma, semestre
-FROM turma;
-
-/*CONSTRAINT 
-FOREIGN KEY ()
-REFERENCES ()*/
-
-
-
-
-
-
